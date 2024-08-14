b790e8ca820b48ab700731296de3cafa462f4608
murilobonetti <mbonetti.dev@gmail.com>
2024-08-14 15:07:07 -0300

    Add docstrings

--- api/endpoints/accounts.py
+++ api/endpoints/accounts.py
@@ -8,25 +8,46 @@ accounts = Accounts()
 
 @app.route('/balance', methods=['GET'])
 def get_balance():
+    """
+    Gets the balance from a specific account.
+
+    :return: Account balance with Response code 200 or 404.
+    """
     account_id = request.args.get("account_id")
     result = accounts.get_balance(account_id)
 
     if result is None:
-        # If account does not exist, returns 404 response code with balance 0.
         return jsonify(0), 404
     else:
-        # Returns account balance with 200 response code.
         return jsonify(result), 200
 
 
 @app.route('/reset', methods=['POST'])
 def reset():
+    """
+    Resets the API to its original state.
+
+    :return: OK with Response code 200.
+    """
     accounts.reset_state()
     return jsonify("OK"), 200
 
 
 @app.route('/event', methods=['POST'])
 def handle_events():
+    """
+    Handles the following events:
+
+    - Account creation
+
+    - Deposit
+
+    - Withdraw
+
+    - Transfer
+
+    :return: Returns a JSON with the result of the transaction or an error.
+    """
     event = request.get_json()
 
     if "type" not in event:
--- api/services/account_service.py
+++ api/services/account_service.py
@@ -10,16 +10,42 @@ class Accounts:
         ]
 
     def add_account(self, account_id, balance):
+        """
+           Add a new account to the accounts list.
+
+           :param:
+           - account_id (str): Id of the account that should be created.
+           - balance (int): Initial balance of the account.
+        """
         new_account = {"id": account_id, "balance": balance}
         self.accounts.append(new_account)
 
     def get_account(self, account_id):
+        """
+           Look for the account with the provided account_id in the accounts
+           list.
+
+           :param:
+           - account_id (str): Id of the account you want to retrieve.
+
+           :returns: Account (dict) or None
+        """
         for account in self.accounts:
             if account["id"] == account_id:
                 return account
         return None
 
     def update_balance(self, account_id, amount, is_deposit):
+        """
+           Updates the balance of an account.
+
+           :param:
+           - account_id (str): Id of the account you want to retrieve.
+           - amount (int): Amount that should be added or subtracted.
+           - is_deposit (bool): Flag that indicates if it's a deposit or withdrawal.
+
+           :returns: Account's balance (int) or None.
+        """
         account = self.get_account(account_id)
         if account:
             if is_deposit:
@@ -31,6 +57,25 @@ class Accounts:
         return None
 
     def process_event(self, event):
+        """
+           Process the event received. This event can be the following:
+
+               **Deposit:** An event type "deposit" can execute two different transactions.
+               Create a new account if it does not exist or update
+               the balance of an existing account.
+
+               **Withdraw:** An event type "withdraw" will subtract the received amount
+               from the "origin" account.
+
+               **Transfer:** An event type "transfer" will update the balance of the
+               "origin" account subtracting the amount received in the event and
+               adding the same amount to the "destination" account.
+
+           :parameter:
+           - event (str): A JSON with the details of the transaction.
+
+           :returns: dict or None.
+        """
         event_type = event.get("type")
         destination = event.get("destination")
         origin = event.get("origin")
@@ -46,6 +91,17 @@ class Accounts:
             return {"error": "Something went wrong!"}
 
     def deposit_amount(self, account_id, amount):
+        """
+            Make a deposit into an account. If the account does not exist
+            creates one with the *amount* being the initial balance, otherwise,
+            adds the *amount* received to the account.
+
+            :param:
+            - account_id (str): Id of the account to make the deposit.
+            - amount (int): Amount that should be added.
+
+            :returns: result (dict) or None.
+        """
         account = self.get_account(account_id)
 
         if account:
@@ -64,9 +120,18 @@ class Accounts:
         return result
 
     def withdraw_amount(self, account_id, amount):
+        """
+            Make a withdrawal from an account subtracting the *amount* received
+            from the account.
+
+            :param:
+            - account_id (str): Id of the account to make the deposit.
+            - amount (int): Amount that should be added.
+
+            :returns: result (dict) or None.
+        """
         account = self.get_account(account_id)
 
-        # Check if account exists, if not returns None
         if account:
             new_balance = self.update_balance(
                 account_id,
@@ -86,10 +151,20 @@ class Accounts:
         return None
 
     def transfer_amount(self, origin_id, amount, destination_id):
+        """
+            Make a transfer from an account to another subtracting the *amount* received
+            from the *origin* account to the *destination* account.
+
+            :param:
+            - origin_id (str): Id of the origin account.
+            - amount (int): Amount that should be transfered.
+            - destination_id (str): Id of the destination account.
+
+            :returns: result (dict) or None.
+        """
         origin_account = self.get_account(origin_id)
         destination_account = self.get_account(destination_id)
 
-        # Check if both accounts exist, if not returns None
         if origin_account is None or destination_account is None:
             return None
 
@@ -110,6 +185,14 @@ class Accounts:
         return result
 
     def get_balance(self, account_id):
+        """
+            Gets the balance from an account if it exists.
+
+            :param:
+            - account_id (str): Id of the account to get the balance.
+
+            :returns: Account's balance (int) or None.
+        """
         account = self.get_account(account_id)
 
         # Check if account exists and returns its balance, otherwise returns None
